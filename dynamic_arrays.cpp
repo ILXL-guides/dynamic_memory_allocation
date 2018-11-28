240dee28ac3060e24a78b2aa4eb90b67313b5fc9
Paul Inventado <pinventado@fullerton.edu>
2018-11-28 11:21:07 -0800

    Add comments that explain the code

--- dynamic_arrays.cpp
+++ dynamic_arrays.cpp
@@ -3,27 +3,31 @@
 using namespace std;
 
 int main() {
-  int* values = new int[10];
-  student* cpsc121 = new student[60];
+  int* values = new int[10]; // dynamic array of int
+  student* cpsc121 = new student[60]; // dynamic array of student
   int i = 1;
 
-  values[0] = 5;
-  *(values + i) = 6;
+  values[0] = 5; // assigning values using bracket notation
+  *(values + i) = 6; // assigning values using pointer arithmetic
 
+  // calling object member functions using bracket notation
   cpsc121[0].set_name("Julian");
   cpsc121[0].set_id(12345);
   cpsc121[0].display();
 
+  // calling object member functions using pointer arithmetic
   (*(cpsc121+i)).set_name("Adrianna");
   (*(cpsc121+i)).set_id(5678);
   (*(cpsc121+i)).display();
 
   i = 2;
-
+  // calling object member functions using pointer arithmetic and the arrow
+  // notation
   (cpsc121+i)->set_name("Dimeji");
   (cpsc121+i)->set_id(91011);
   (cpsc121+i)->display();
 
+  // deallocating the array
   delete[] values;
   delete[] cpsc121;
 
--- dynamic_pointer_arrays.cpp
+++ dynamic_pointer_arrays.cpp
@@ -3,32 +3,55 @@
 using namespace std;
 
 int main() {
+  // dynamic array of int pointers
   int** value_locations = new int*[10];
+  // dynamic array of student pointers
   student** cpsc121_locations = new student*[10];
   int i = 1;
 
+  // dynamic creation of int stored as an element of the array that is necessary
+  // before assigning or retrieving values
   value_locations[0] = new int;
   *value_locations[0] = 5;
 
+  // pointer arithmetic version of dynamically creating an int and assigning
+  // values
   *(value_locations + i) = new int;
   *(*(value_locations + i)) = 10;
 
+  // Each element is an address that needs to be deleted. We only delete 2
+  // because we only allocated 2 ints
   delete value_locations[0];
   delete value_locations[1];
+  // The array should be deleted only after its individual elements are deleted
   delete[] value_locations;
 
+  // dynamic creation of a student stored as an element of the array that is
+  // necessary before calling its methods
   cpsc121_locations[0] = new student;
+  // The following are variations for accessing the student object's methods
+
+  // pointer arithmetic and bracket notations
   (*cpsc121_locations[0]).set_name("Julian");
+  // bracket notations and arrow notation
   cpsc121_locations[0]->set_id(12345);
   cpsc121_locations[0]->display();
 
+  // using pointer arithmetic to store the dynamically created object
   *(cpsc121_locations + i) = new student;
+  // using pointer arithmetic and the dot notation to access the student
+  // object's methods
   (*(*(cpsc121_locations + i))).set_name("Adriana");
+  // using pointer arithmetic and the arrow notation to access the student
+  // object's methods
   (*(cpsc121_locations + i))->set_id(6789);
   (*(cpsc121_locations + i))->display();
 
+  // Each element is an address that needs to be deleted. We only delete 2
+  // because we only allocated 2 student objects
   delete cpsc121_locations[0];
   delete cpsc121_locations[1];
+  // The array should be deleted only after its individual elements are deleted
   delete[] cpsc121_locations;
 
   return 0;
